55ee5475c7e3fa586cb85d4c56765a945b812af0
Andy Dayton <amdayton@gmail.com>
2015-11-29 08:10:49 -0600

    Added comments and cleaned up

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,9 +3,13 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
 
+    // some global configuration for oF
+    // show all logging info
     ofSetLogLevel(OF_LOG_VERBOSE);
+    // enable anti-aliasing on OpenGL stuff
     ofEnableSmoothing();
 
+    // show HUD by default
     bShowHud = true;
 
     // load image
@@ -46,28 +50,48 @@ void ofApp::setup(){
 //--------------------------------------------------------------
 void ofApp::update(){
 
+    // draw the paint FBO to the mask every frame
+    // (notice that we aren't clearing the fbo, so it accumulates)
     maskFbo.begin();
         paintFbo.draw(0,0);
     maskFbo.end();
 
+    // draw to the main FBO
     mainFbo.begin();
+        // gesso the canvas so we can paint a new frame
         ofClear(0);
+    
+        // draw to the pixelate shader
         pixelate.begin();
+    
+            // set uniforms for our shader!
+    
+            // send textures to pixelate shader
             pixelate.setUniformTexture("src", img.getTexture(), 1);
             pixelate.setUniformTexture("mask", maskFbo.getTexture(), 2);
     
+            // these give basic information about our environment
+            // (many shader tools will provide these automatically
             pixelate.setUniform1f("width", ofGetWidth());
             pixelate.setUniform1f("height", ofGetHeight());
             pixelate.setUniform1f("t", ofGetElapsedTimeMillis());
 
+            // pixel width and height parameters
             pixelate.setUniform1f("pixel_w", pixelWidth);
             pixelate.setUniform1f("pixel_h", pixelHeight);
     
+            // animation parameters
             pixelate.setUniform1i("animation", enableAnimation ? 1 : 0);
             pixelate.setUniform1f("animSpeed", animSpeed);
 
+            // this will control the amount "wetness" of the filter
+            // (how much it is blended)
             pixelate.setUniform1f("blend", blend);
 
+            // draw our image in the shader context
+            // this will by default send a texture uniform `tex0` which
+            // we aren't currently using. it will also provide texture
+            // coordinates to the shader (i think)
             img.draw(0,0);
         pixelate.end();
     mainFbo.end();
@@ -76,36 +100,45 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+    // clear the canvas before w paint a new frame
     ofClear(0);
 
-    // draw image
+    // draw the image at coordinates 0, 0
     img.draw(0,0);
     
-    // draw fbo over image
+    // draw fbo over image (any opacity in the main Fbo will let the
+    // image show through)
     mainFbo.draw(0,0);
     
-    // draw mask if requested
+    // draw mask if enabled
     if(bDrawMask) {
         maskFbo.draw(0,0);
     }
     
     // draw brush
+    // `ofPushStyle` allows us to set styles without
+    // having them applied to the rest of the application
     ofPushStyle();
-        ofEnableAlphaBlending();
-        ofSetColor(255, 255, 244, 100);
+        // set color to be white with partial transparency
+        ofSetColor(255, 255, 255, 100);
+    
         // draw paint fbo
         paintFbo.draw(0,0);
     
         // draw brush outline
         ofNoFill();
         ofDrawEllipse(mouseX, mouseY, brushRadius, brushRadius);
+    
     ofPopStyle();
     
     // draw gui
     gui.draw();
     
+    // draw the HUD if enabled
     if(bShowHud) {
-        string hud = "Press 'c' to clear \nPress 'm' to show mask\nPress 'h' to show/hide this\n";
+        string hud = "Press 'c' to clear \n"
+        hud = hud + "Press 'm' to show mask\n"
+        hud = hud + "Press 'h' to show/hide this\n";
         ofDrawBitmapString(hud, ofGetWidth()-300, 25);
     }
 }
@@ -114,27 +147,37 @@ void ofApp::draw(){
 void ofApp::updatePaintFbo(int x, int y) {
     paintFbo.begin();
         ofClear(0);
+        // draw an ellipse to the paint fbo at the mouse position
         ofDrawEllipse(x, y, brushRadius, brushRadius);
     paintFbo.end();
 }
 
 void ofApp::clearPaintFbo() {
+
+    // clear out the paint fbo
     paintFbo.begin();
         ofClear(0);
     paintFbo.end();
 }
 void ofApp::clearMaskFbo() {
+
+    // clear out the mask fbo
     maskFbo.begin();
         ofClear(0);
     maskFbo.end();
 }
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
+
+    // clear the mask if "c" key is pressed
     if(key == 'c') {
-        ofLogVerbose("Clear mask");
         clearMaskFbo();
+        
+    // show the mask if "m" key is pressed
     } else if (key == 'm') {
         bDrawMask = true;
+        
+    // toggle the HUD if "h" key is pressed
     } else if (key == 'h') {
         bShowHud = !bShowHud;
     }
@@ -142,8 +185,8 @@ void ofApp::keyPressed(int key){
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
+    // disable mask display
     bDrawMask = false;
-//    useEraser = 0;
 }
 
 //--------------------------------------------------------------
@@ -153,16 +196,19 @@ void ofApp::mouseMoved(int x, int y ){
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
+    // update paint fbo when mouse is dragged
     updatePaintFbo(x, y);
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
+    // update paint fbo when mouse is pressed
     updatePaintFbo(x, y);
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
+    // clear paint fbo when mouse is released
     clearPaintFbo();
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -7,14 +7,25 @@
 class ofApp : public ofBaseApp{
 
 	public:
-		void setup();
-		void update();
-		void draw();
     
-        void updatePaintFbo(int x, int y);
-        void clearPaintFbo();
-        void clearMaskFbo();
+    /***********************************************************************
+     * Standard openFrameworks methods
+     *
+     * these are provided by oF and present in every oF app
+     *
+     **********************************************************************/
+    
+        // default oF method that runs when the application opens
+		void setup();
+    
+        // default oF method that runs every frame
+        // this is more for CPU-based calculations etc
+		void update();
+    
+        // default oF method that draws to the screen
+		void draw();
 
+        // these are various oF event functions
 		void keyPressed(int key);
 		void keyReleased(int key);
 		void mouseMoved(int x, int y );
@@ -26,20 +37,49 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+    /***********************************************************************
+     * Custom methods and members
+     *
+     * these are specific to this app
+     *
+     **********************************************************************/
+
+        // this method draws a circle to the paint Frame Buffer Object
+        // centered on the mouse position
+        void updatePaintFbo(int x, int y);
     
+        // erase the paint Frame Buffer Object
+        void clearPaintFbo();
+    
+        // erase the mask Frame Buffer Object
+        void clearMaskFbo();
+    
+        // toggle for showing/hiding the HUD menu
         bool bShowHud;
+        // toggle for showing/hiding the mask
         bool bDrawMask;
     
-    
+        // this is our main source image
         ofImage img;
     
+        // shader for applying the pixelation effect
         ofShader pixelate;
+        // shader used to mask the pixelation effect
         ofShader mask;
+    
+        // our main Frame Buffer object where masked pixelation
+        // effect is applied
         ofFbo mainFbo;
+        // the fbo that stores our alpha mask
         ofFbo maskFbo;
+        // the fbo that stores a single frame of painting
         ofFbo paintFbo;
     
-    
+        // GUI parameters
+        // these not only show up in GUI, but are used as members
+        // to store parameters
+        
         ofxPanel gui;
 		ofxFloatSlider pixelWidth;
         ofxFloatSlider pixelHeight;
